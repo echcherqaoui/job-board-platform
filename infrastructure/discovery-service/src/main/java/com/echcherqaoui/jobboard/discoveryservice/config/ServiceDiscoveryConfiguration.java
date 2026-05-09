477fda9d3e975056fc31c39b1883af872df1fffd
Khalid <kechcherqaoui@xperlean.com>
2026-05-09 09:01:02 +0100

    chore: initial project setup for job-board microservices

---
+++ infrastructure/discovery-service/src/main/java/com/echcherqaoui/jobboard/discoveryservice/config/ServiceDiscoveryConfiguration.java
@@ -0,0 +1,9 @@
+package com.echcherqaoui.jobboard.discoveryservice.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class ServiceDiscoveryConfiguration {
+}
---
+++ services/notification-service/src/test/java/com/echcherqaoui/jobboard/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echcherqaoui.jobboard.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
